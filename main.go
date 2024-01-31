9b6e1ae93ddbb3a395000c66604f8dab9e6447df
renat <renat-fr@yandex.ru>
2024-01-31 16:58:40 +0300

    first

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type User struct {
+	name                  string
+	age                   uint16
+	money                 int16
+	avg_grades, happiness float64
+}
+
+func (user *User) getAllInfo() string {
+	return fmt.Sprintf("User name is: %s, Age is %d, Money is %d", user.name, user.age, user.money)
+}
+
+func (user *User) setNewName(name string) {
+	user.name = name
+}
+
+func main() {
+	handlerRequest()
+}
+
+func home_page(w http.ResponseWriter, r *http.Request) {
+	bob := User{"Bob", 25, -50, 4.2, 0.8}
+	bob.setNewName("Renat")
+	fmt.Fprintf(w, bob.getAllInfo())
+	//tmpl, _ := teamplate.parseFiles()
+
+}
+
+func handlerRequest() {
+	http.HandleFunc("/", home_page)
+	http.HandleFunc("/contacts/", contacts_page)
+	http.ListenAndServe(":8080", nil)
+}
+
+func contacts_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Contacts")
+}
